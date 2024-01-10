39eb87ad74a9b910b11a82e886106b6063527fa5
Nathan <11278700+nathansnellaert@users.noreply.github.com>
2024-01-10 14:15:17 +0100

    Update SDK to use new simpler API

--- README.md
+++ README.md
@@ -8,21 +8,10 @@ Run the following command to install the API:
 
 ```pip install subsetsio```
 ## Usage
-
-At the moment, you can only use the SDK for querying. To explore datasets, checking quotas, and viewing past queries, visit the [subsets.io](https://www.subsets.io) web interface.
-
 ```python
-import subsets
+import subsetsio
 
-# Temp API Key. Sign up for a free permanent key.
-client = subsets.client(api_key="YOUR_API_KEY")
-
-user_query = "apple stock price"
-# Search for tables, and rows within the table
-response = client.search_tables(user_query, n=3)
-# TODO: implement LLM function to convert text to sql
-sql_query = text2sql(user_query, response.tables)
-# Execute the query
+client = subsetsio.Client(api_key="YOUR_API_KEY")
 df = client.query(sql_query)
 ```
 
--- subsetsio/__init__.py
+++ subsetsio/__init__.py
@@ -1,23 +1,17 @@
 import requests
+import pandas as pd
 
 class Client:
-    def __init__(self, api_key, base_url='https://server-2cmmob2q7a-ez.a.run.app'):
-        self.api_key = api_key
-        self.base_url = base_url
+    def __init__(self, api_key: str, base_url: str = 'http://services.subsets.io'):
+        self.api_key: str = api_key
+        self.base_url: str = base_url
 
-    def search_tables(self, query, n=3):
-        response = requests.get(
-            f"{self.base_url}/deep_search",
-            params={'query': query, 'n': n, 'api_key': self.api_key}
-        )
-        response.raise_for_status()
-        return response.json()
-
-    def query(self, sql_query):
+    def query(self, sql_query: str) -> pd.DataFrame:
         data = {'query': sql_query, 'api_key': self.api_key}
         response = requests.post(
-            f"{self.base_url}/execute",
+            f"{self.base_url}/sql/query",
             json=data
         )
         response.raise_for_status()
-        return response.json()
+        response_body = response.json()
+        return pd.DataFrame(columns=response_body['columns'], data=response_body['data'])
